a6dc482e075aa4edfa9c09dea8f31aa4662979be
Johnna Anderson <jlanderson.1939@gmail.com>
2023-08-23 10:33:32 -0400

    listusers

---
+++ server/src/main/java/com/techelevator/tenmo/model/UserName.java
@@ -0,0 +1,14 @@
+package com.techelevator.tenmo.model;
+
+public class UserName {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
