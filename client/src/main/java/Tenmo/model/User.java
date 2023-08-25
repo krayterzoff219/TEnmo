4fec1bdbf37d1f1132a36e4dc5e7b3423ff3dd53
Timothy Buchak <timothy.buchak@gmail.com>
2023-08-25 09:41:44 -0400

    Some added ConsoleService

--- client/src/main/java/Tenmo/model/User.java
+++ client/src/main/java/Tenmo/model/User.java
@@ -1,4 +1,25 @@
 package Tenmo.model;
 
+import java.math.BigDecimal;
+
 public class User {
+
+    private String username;
+    private BigDecimal balance;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
